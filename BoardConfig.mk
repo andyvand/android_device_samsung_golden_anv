3f232bb62ed2d5a68d776fd46fa58644a26d2f03
Andy Vandijck <andyvand@gmail.com>
2016-02-18 15:43:35 +0100

    CyanogenMod 13.0 Golden

---
+++ BoardConfig.mk
@@ -0,0 +1,185 @@
+#
+# Copyright (C) 2014 NovaFusion http://novafusion.pl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release Build
+CM_BUILDTYPE := RELEASE
+
+# Architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a9
+CPU_TARGET_SMP := true
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5064622080
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Board
+TARGET_BOOTLOADER_BOARD_NAME := montblanc
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Platform 
+TARGET_BOARD_PLATFORM := montblanc
+BOARD_USES_STE_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DSTE_HARDWARE -DSTE_SAMSUNG_HARDWARE 
+BOARD_LPM_BOOT_ARGUMENT_NAME := lpm_boot
+BOARD_LPM_BOOT_ARGUMENT_VALUE := 1
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+#TARGET_PREBUILT_KERNEL := device/samsung/golden/ramdisk/zImage
+TARGET_KERNEL_SOURCE := kernel/samsung/golden
+TARGET_KERNEL_CONFIG := golden_android_defconfig
+TARGET_NO_INITLOGO := true
+
+# Graphics
+USE_OPENGL_RENDERER := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+BOARD_EGL_CFG := device/samsung/golden/prebuilt/lib/egl/egl.cfg
+COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH -DWORKAROUND_BUG_10194508
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+BOARD_HAVE_PRE_KITKAT_AUDIO_BLOB := true
+BOARD_HAVE_PRE_KITKAT_AUDIO_POLICY_BLOB := true
+COMMON_GLOBAL_CFLAGS += -DMR0_AUDIO_BLOB -DMR1_AUDIO_BLOB
+USE_LEGACY_AUDIO_POLICY := 1
+BOARD_USES_LEGACY_MMAP := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/golden/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/golden/bluetooth/btvendor_golden.txt
+
+# Wi-Fi related defines
+BOARD_WLAN_DEVICE := bcmdhd
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/system/etc/wifi/bcmdhd_p2p.bin"
+WIFI_DRIVER_MODULE_NAME := "dhd"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_AP_ARG := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+BOARD_NO_APSME_ATTR := true
+
+# Misc
+BOARD_RIL_CLASS := ../../../device/samsung/golden/ril/
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/golden/include
+TARGET_RECOVERY_FSTAB := device/samsung/golden/ramdisk/fstab.samsunggolden
+
+# GPS
+TARGET_ENABLE_NON_PIE_SUPPORT := true
+
+
+# Parameters
+OPTFLAGS_ARM := -march=armv7-a -mtune=cortex-a9 -Ofast -flto
+WITH_DEXPREOPT := true
+OVERRIDE_RUNTIMES := runtime_libdvm_default runtime_libart
+
+# GCC 5.3.0 & Java 1.8
+TARGET_TOOLS_PREFIX := /usr/bin/arm-linux-androideabi-
+KERNEL_TOOLS_PREFIX := /usr/bin/arm-eabi-
+ARM_EABI_PREFIX := /usr/bin/arm-eabi-
+JAVA_HOME := /usr/java/jdk1.8.0_72
+TARGET_GCC_EXP := 5.3
+
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+#TARGET_PREBUILT_KERNEL := device/samsung/golden/ramdisk/zImage
+TARGET_KERNEL_SOURCE := kernel/samsung/golden
+TARGET_KERNEL_CONFIG := golden_cm13_defconfig
+TARGET_NO_INITLOGO := true
+
+# NFC
+BOARD_NFC_CHIPSET := pn547
+BOARD_NFC_LPM_LOSES_CONFIG := true
+BOARD_NFC_DEVICE := "/dev/pn547"
+
+# OTA Parameters
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/golden/include
+TARGET_OTA_ASSERT_DEVICE := golden,i8190,GT-I8190
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 25
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+
+# RIL
+BOARD_USES_LIBSECRIL_STUB := true
+BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/golden/recovery/recovery_kernel
+TARGET_RECOVERY_INITRC = device/samsung/golden/recovery/root/init.rc
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/golden/recovery/graphics.c
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+BOARD_HAS_NO_MISC_PARTITION := true
+
+# TWRP Stuffs
+TW_BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/golden/recovery/twrp-graphics.c
+DEVICE_RESOLUTION := 480x800
+RECOVERY_SDCARD_ON_DATA := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/virtual/android_usb/android0/f_mass_storage/lun/file
+
+TW_INCLUDE_CRYPTO_SAMSUNG := true
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p25"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "noatime,nosuid,nodev,discard,noauto_da_alloc,journal_async_commit,errors=panic    wait,check"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+
+# TWRP Extras
+TW_INCLUDE_NTFS_3G := true
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USERIMAGES_USE_F2FS := true
+TWHAVE_SELINUX := true
+TW_INCLUDE_FB2PNG := true
+TW_INCLUDE_INJECTTWRP := true
+TARGET_USES_LOGD := true
+TWRES_PATH := "/res"
+TW_DEFAULT_LANGUAGE := nl
+
---
+++ cm.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2014 NovaFusion http://novafusion.pl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Screen
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/golden/golden.mk)
+
+# Inherit some common CM stuff
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# NFC Extras
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Device specific
+PRODUCT_NAME := cm_golden
+PRODUCT_PACKAGES += Torch \
+    Superuser \
+    com.android.future.usb.accessory
+
